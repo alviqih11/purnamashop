65cf6da483f2509cea1b7b834a7ba67a6136976d
ihsannurhuda <ciekepoyah@gmail.com>
2022-05-11 17:09:58 +0700

    purnama shop

---
+++ script.js
@@ -0,0 +1,8 @@
+const toggle = document.querySelector('.toggle');
+const isiToggle = document.querySelector('.isi-toggle');
+
+
+toggle.addEventListener('click', function () {
+    isiToggle.classList.toggle('active');
+})
+
