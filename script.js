286b68e00e86c0528667881c1f5e300406de93e6
alviqih11 <93898947+alviqih11@users.noreply.github.com>
2022-05-12 17:24:25 +0700

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const toggle = document.querySelector('.toggle');
-const isiToggle = document.querySelector('.isi-toggle');
-
-
-toggle.addEventListener('click', function () {
-    isiToggle.classList.toggle('active');
-})
-
